407dd1ee78c4e73eccacd9a9fe8f79a30fa15b3f
Aluno_AEDS <Aluno_AEDS@DESKTOP-RVOTSDK>
2023-11-22 14:10:15 +0000

    Adicionar arquivos de projeto.

---
+++ Exercício 11/Form1.cs
@@ -0,0 +1,15 @@
+namespace Exercício_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void corETipoDeLetraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Font 
+        }
+    }
+}
